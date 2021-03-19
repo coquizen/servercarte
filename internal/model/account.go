4363732c6acf170351d4567da89f99792c4047e7
Ian Canino <ian@thecommittedbug.io>
2021-03-19 11:53:15 -0400

    add belongs to user relationship

--- internal/model/account.go
+++ internal/model/account.go
@@ -1,11 +1,30 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"time"
 
+	"github.com/google/uuid"
+)
+
+
+type AccessLevel int
+
+const (
+	Admin AccessLevel = iota
+	Employee
+	Guest
+)
+
+// Account contains a user's account properties for interacting with the system
 type Account struct {
 	Base
-	Username string `json:"username" gorm:"not null"`
-	Password string `json:"password" gorm:"not null"`
-	UserID uuid.UUID `json:"userID" gorm:"not null"`
-	Role string `json:"role" gorm:"not null"`
+	UserID      uuid.UUID   `json:"userID" gorm:"not null"`
+	User        User        `comment:"account BELONGS TO user"`
+	Username    string      `json:"username" gorm:"not null"`
+	Password    string      `json:"password" gorm:"not null"`
+	Role        AccessLevel `json:"role" gorm:"not null"`
+	Token       string      `json:"token,omitempty" gorm:"null"`
+	TokenExpiry time.Time   `json:"token_expiry,omitempty" gorm:"null"`
+	LastLogin   time.Time   `json:"last_login,omitempty" gorm:"null"`
 }
+
