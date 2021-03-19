8c3753cedf477ee5d6a80e3af13ec4cee67c9632
Ian Canino <ian@thecommittedbug.io>
2021-03-19 11:39:16 -0400

    fix formatting

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,22 +3,10 @@ package model
 // User struct represent the user that will have admin access.
 type User struct {
 	Base
-	FirstName string  `json:"first_name,omitempty" gorm:"unique,not null"`
-	LastName  string  `json:"last_name,omitempty" gorm:"unique,not null"`
-	Addr   string  `json:"address" gorm:"not null"`
-	ZipCode   uint    `json:"zip_code" gorm:"not null"`
-	Email     string  `json:"email,omitempty" gorm:"unique,not null"`
-	Account   Account `json:"-" gorm:"not null;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	FirstName string `json:"first_name" gorm:"unique,not null"`
+	LastName  string `json:"last_name,omitempty" gorm:"unique,null"`
+	Address1  string `json:"address_1" gorm:"not null"`
+	Address2  string `json:"address_2,omitempty" gorm:"null"`
+	ZipCode   uint   `json:"zip_code" gorm:"not null"`
+	Email     string `json:"email" gorm:"unique,not null"`
 }
-
-// // BeforeSave is a gorm hook that runs before a new row is inserted into the
-// // db; In particular, hash the password before inserting into the
-// // db.
-// func (u *User) BeforeSave(tx *gorm.DB) error {
-// 	passwordHash, err := security.Hash(u.Password)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	u.Password = string(passwordHash)
-// 	return nil
-// }
